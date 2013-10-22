62814bd9e4610976c8c8513fc799d42824fc4cdc
Charles Cary <cclego@gmail.com>
2013-10-21 19:12:41 -0500

    first working draft of DAL code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# fill in these with values provided by the instructor
+export AWS_SECRET_KEY=<secret key here>
+export AWS_ACCESS_KEY=<access key here>
+
+# update packages
 apt-get update
 
-# basic stuff
+# core stuff
 apt-get install -y git-core
 apt-get install -y supervisor
 apt-get install -y vim
+apt-get install -y zip
 
 # python setup
 apt-get install -y python-dev
@@ -33,3 +39,22 @@ if [ ! -d "/vagrant/lsdacluster" ]; then
   mkdir /vagrant/lsdacluster
   chmod 777 /vagrant/lsdacluster
 fi
+
+# setup DAL
+pip install boto
+pip install czipfile
+git clone https://github.com/cioc/DAL.git /home/vagrant/DAL
+cd  /home/vagrant/DAL
+python setup.py install
+if [ ! -d "/vagrant/dalcache" ]; then
+  mkdir /vagrant/dalcache
+  chmod 777 /vagrant/dalcache
+fi
+rm /vagrant/dalcache/*
+cp /vagrant/sou.zip /vagrant/dalcache/sou.zip
+cd /vagrant/dalcache
+unzip sou.zip
+touch /root/.dalconfig
+echo '{"checkpoint": {"bucket": ""}, "system": {"local" : false}, "cache": {"AWS_SECRET_KEY": "'$AWS_SECRET_KEY'", "path": "/vagrant/dalcache", "AWS_ACCESS_KEY": "'$AWS_ACCESS_KEY'", "size": "10G"}, "rcv1": {"bucket": "ml-rcv1"}, "crime": {"bucket": "ml-crime"}, "ngrams": {"bucket": "ml-ngrams"}, "wishes": {"bucket": "ml-wishes"}, "sou": {"bucket": "ml-sou"}, "tinyimages": {"meta-bucket": "ml-tinyimages-metadata", "bucket": "ml-tinyimages"}}' > /root/.dalconfig
+
+# end of script
